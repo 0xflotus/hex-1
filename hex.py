0dd810e53d2df72e10333adc1e64e8eed3f24fa8
0xflotus <0xflotus@gmail.com>
2022-01-27 11:35:35 +0100

    Update hex.py

--- hex.py
+++ hex.py
@@ -9,9 +9,10 @@ import signal
 import sys
 import shutil
 
+from stringcolor import *
 
 # Application version number.
-__version__ = '2.2.0'
+__version__ = "2.3.0"
 
 
 # Command line help text.
@@ -33,7 +34,9 @@ Flags:
   -h, --help            Display this help text and exit.
   -v, --version         Display version number and exit.
 
-""" % os.path.basename(sys.argv[0])
+""" % os.path.basename(
+    sys.argv[0]
+)
 
 
 # Custom argparse 'action' to print our own help text instead of the default.
@@ -45,7 +48,7 @@ class HelpAction(argparse.Action):
 
 # Suppress 'broken pipe' warnings when piping output through pagers.
 # This resolves the issue when running under Cygwin on Windows.
-if hasattr(signal, 'SIGPIPE'):
+if hasattr(signal, "SIGPIPE"):
     signal.signal(signal.SIGPIPE, signal.SIG_DFL)
 
 
@@ -59,29 +62,39 @@ def write(s):
         sys.exit()
 
 
+# Colorize the output
+def colorize(text, color):
+    return f"\033[38;5;{color}m" + text + "\u001b[0m"
+
+
 # Print a single line of output to stdout.
 def writeln(offset, buffer, bytes_per_line, line_num_width):
 
     # Write the line number.
-    write('% *X \u001B[90m│\u001B[0m' % (line_num_width, offset))
+    write("% *X \u001B[90m│\u001B[0m" % (line_num_width, offset))
 
     for i in range(bytes_per_line):
 
         # Write an extra space in front of every fourth byte except the first.
         if i > 0 and i % 4 == 0:
-            write(' ')
+            write(" ")
 
         # Write the byte in hex form, or a spacer if we're out of bytes.
-        write(' %02X' % buffer[i] if i < len(buffer) else '   ')
+        write(
+            colorize(
+                " 0x%02x" % buffer[i] if i < len(buffer) else "     ",
+                buffer[i] if i < len(buffer) else 0,
+            )
+        )
 
-    write('\u001B[90m │ \u001B[0m')
+    write("\u001B[90m │ \u001B[0m")
 
     # Write a character for each byte in the printable ascii range.
     for i in range(len(buffer)):
-        nonprintable = '\u001B[90m·\u001B[0m'
-        write('%c' % buffer[i] if 32 <= buffer[i] <= 126 else nonprintable)
+        nonprintable = "\u001B[90m·\u001B[0m"
+        write("%c" % buffer[i] if 32 <= buffer[i] <= 126 else nonprintable)
 
-    write('\n')
+    write("\n")
 
 
 # Dump the specified file to stdout.
@@ -92,11 +105,11 @@ def dump(file, offset, bytes_to_read, bytes_per_line, line_num_width):
         if file.seekable():
             file.seek(offset)
         else:
-            sys.exit('Error: %s is not seekable.' % file.name)
+            sys.exit("Error: %s is not seekable." % file.name)
 
     # Print a line.
     cols, _ = shutil.get_terminal_size()
-    print('\u001B[90m' + '─' * cols + '\u001B[0m')
+    print("\u001B[90m" + "─" * cols + "\u001B[0m")
 
     # Read and dump one line per iteration.
     while True:
@@ -124,7 +137,7 @@ def dump(file, offset, bytes_to_read, bytes_per_line, line_num_width):
         else:
             break
 
-    print('\u001B[90m' + '─' * cols + '\u001B[0m')
+    print("\u001B[90m" + "─" * cols + "\u001B[0m")
 
 
 def main():
@@ -135,55 +148,74 @@ def main():
 
     # The filename argument is optional. We default to reading from
     # stdin if it's omitted.
-    parser.add_argument('file',
-        nargs='?',
-        help='file to dump (default: stdin)',
-        type=argparse.FileType('rb'),
+    parser.add_argument(
+        "file",
+        nargs="?",
+        help="file to dump (default: stdin)",
+        type=argparse.FileType("rb"),
         default=sys.stdin.buffer,
     )
 
     # Flags.
-    parser.add_argument('-h', '--help',
-        action = HelpAction,
+    parser.add_argument(
+        "-h",
+        "--help",
+        action=HelpAction,
         nargs=0,
-        help = 'print this help message and exit',
+        help="print this help message and exit",
     )
-    parser.add_argument('-v', '--version',
-        action='version',
+    parser.add_argument(
+        "-v",
+        "--version",
+        action="version",
         version=__version__,
+    )
+    parser.add_argument(
+        "-c",
+        "--colorize",
+        help="colorize the output",
+        action="store_false",
     )
 
     # Options.
-    parser.add_argument('-l', '--line',
-        help='bytes per line in output (default: 16)',
+    parser.add_argument(
+        "-l",
+        "--line",
+        help="bytes per line in output (default: 16)",
         default=16,
         type=int,
-        dest='bpl',
+        dest="bpl",
     )
-    parser.add_argument('-n', '--number',
-        nargs='?',
-        help='number of bytes to read (default: 256)',
+    parser.add_argument(
+        "-n",
+        "--number",
+        nargs="?",
+        help="number of bytes to read (default: 256)",
         type=int,
         default=-1,
         const=256,
-        dest='btr',
+        dest="btr",
     )
-    parser.add_argument('-o', '--offset',
-        help='offset at which to begin reading (default: 0)',
+    parser.add_argument(
+        "-o",
+        "--offset",
+        help="offset at which to begin reading (default: 0)",
         type=int,
         default=0,
-        dest='offset',
+        dest="offset",
     )
-    parser.add_argument('-w', '--width',
-        help='line number width (default: 6)',
+    parser.add_argument(
+        "-w",
+        "--width",
+        help="line number width (default: 6)",
         type=int,
         default=6,
-        dest='num_width',
+        dest="num_width",
     )
 
     args = parser.parse_args()
     dump(args.file, args.offset, args.btr, args.bpl, args.num_width)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
